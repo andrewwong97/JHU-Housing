6d4b05fe5405b323419a76eadb754a4b1aa9bb12
andrewwong97 <aznhippos@gmail.com>
2016-03-18 18:06:02 -0400

    now writes to file

--- housing.py
+++ housing.py
@@ -9,20 +9,27 @@ def call():
 	priorities = ["14","00","15","05","13","10","11","21","24","12","18","26","27"]
 	print "Priority Low to High"
 	priorities.reverse()
+	timenow = time.strftime("%H_%M_%S")
 	for num in priorities:
-		search(rows, num)
+		search(rows, num, timenow)
+		
+	print "Current time is: " + time.strftime("%H:%M:%S")
+
+	with open(timenow+".txt", 'a') as f:
+		f.write("Current time is: " + time.strftime("%H:%M:%S"))
+
 	
 
-def search(rows, num):
-	for tr in rows:
-		cols = tr.findAll('td')
-		if "11 month" not in cols[4].text:
-			for td in cols:
-				if "Commons" in td.text and len(td.text)<25 and num in td.text and td.text[len(td.text)-1]=="A":
-					print td.text[18:-1]
+def search(rows, num, timenow):
+	with open(timenow+".txt", 'a') as f:
+		for tr in rows:
+			cols = tr.findAll('td')
+			if "11 month" not in cols[4].text:
+				for td in cols:
+					if "Commons" in td.text and len(td.text)<25 and num in td.text and td.text[len(td.text)-1]=="A":
+						print td.text[18:-1]
+						f.write(td.text[18:-1]+"\n")
 
 while (True):
 	call()
-	print "Current time is: " + time.strftime("%H:%M:%S")
-	time.sleep(30)
-	
+	time.sleep(10)
